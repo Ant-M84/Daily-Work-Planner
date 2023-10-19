9c8e6e4074a1a68df668984ea5269a9257dbbcf3
Ant_M84 <anthony.l.michel@gmail.com>
2023-10-19 18:42:14 -0400

    updated html, css and js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,49 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function () {
+
+  $(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, task);
+  });
+
+  $(".time-block").each(function () {
+    var workHour = parseInt($(this).attr("id").split("hour")[1]);
+    var currentTime =  dayjs().hour;
+    if (workHour === currentTime) {
+    // $(this).removeClass("past");
+    //  $(this).removeClass("future");
+      $(this).addClass("present");
+    }
+    else if (workHour < currentTime) {
+     //  $(this).removeClass("present");
+     // $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+    else {
+    //  $(this).removeClass("present");
+    //  $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  })
+
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+
+  function displayDate() {
+    var today = dayjs().format("dddd, MMMM D YYYY, h:mm:ss a"); 
+    $("#currentDay").text(today);
+    }
+    displayDate();
+    setInterval(displayDate, 1000);
+
+  $(".clearBtn").on("click", function() {
+  localStorage.clear();
+  location.reload();
+    })
 });
